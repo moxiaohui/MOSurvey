3efa28511dd813a30bb1fcaf8f101bfb7919ef8e
xhmo <xhmo@mobvoi.com>
2021-01-14 10:32:54 +0800

    fixed MOProxy.m

--- MOSurveyOC/MOSurveyOC/MOTimer/MOProxy.m
+++ MOSurveyOC/MOSurveyOC/MOTimer/MOProxy.m
@@ -9,12 +9,18 @@
 
 @implementation MOProxy
 
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel {
-  return [self.target methodSignatureForSelector:sel];
+// 消息转发的第二次机会
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+  return _target; // 如果target为nil，则触发第三次机会
 }
 
+// 消息转发的第三次机会
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel {
+  return [NSObject instanceMethodSignatureForSelector:@selector(init)]; // 获取了init方法的方法签名
+}
 - (void)forwardInvocation:(NSInvocation *)invocation {
-  [invocation invokeWithTarget:self.target];
+  void *null = NULL;
+  [invocation setReturnValue:&null]; // 给nil发送init方法，不会crash
 }
 
 @end
