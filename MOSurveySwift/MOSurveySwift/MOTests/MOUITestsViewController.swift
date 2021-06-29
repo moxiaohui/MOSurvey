aa6dfd6b8e80241d519ce1f0d9f02cbb557c3a0c
mikimo <mikimo@tencent.com>
2021-06-29 20:19:48 +0800

    Deleted MOUITestDemo

--- MOSurveySwift/MOSurveySwift/MOTests/MOUITestsViewController.swift
+++ MOSurveySwift/MOSurveySwift/MOTests/MOUITestsViewController.swift
@@ -11,7 +11,6 @@ let gSubscribeButtonAccessibilityIdentifier = "MOUITestsViewController_subscribe
 
 class MOUITestsViewController: UIViewController {
 
-    /// MOUITestsViewControllerUITests (相关)
     public var isCanTests: Bool = false
     var subscribeButton: UIButton = {
         let btn = UIButton(type: .custom)
@@ -25,39 +24,13 @@ class MOUITestsViewController: UIViewController {
     let num1: CGFloat = 1.0
     let num2: CGFloat = 2.0
     
-    /// MOUITestDemo (相关)
-    let textField: UITextField = {
-        let field = UITextField(frame: .zero)
-        field.backgroundColor = .cyan
-        field.accessibilityLabel = "textField"
-        return field
-    }()
-    let sendButton: UIButton = {
-        let btn = UIButton(type: .custom)
-        btn.setTitle("Send", for: .normal)
-        btn.setTitleColor(.black, for: .normal)
-        btn.accessibilityLabel = "sendButton"
-        return btn
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        // MOUITestsViewControllerUITests (相关)
         view.addSubview(subscribeButton)
-        
-        // MOUITestDemo (相关)
-        view.addSubview(textField)
-        textField.frame = CGRect(x: 50, y: 200, width: 200, height: 50)
-        
-        sendButton.frame = CGRect(x: 260, y: 200, width: 60, height: 50)
-        sendButton.addTarget(self, action: #selector(clickSend), for: .touchUpInside)
-        view.addSubview(sendButton)
-
     }
     
-    // MARK: - MOUITestsViewControllerUITests (相关)
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.subscribeButton.frame = CGRect(x: 10.0, y: 100.0, width: 100.0, height: 50.0)
@@ -66,11 +39,5 @@ class MOUITestsViewController: UIViewController {
     @objc func clickSubscribeButton(sender: UIButton) {
         sender.isSelected = !sender.isSelected
     }
-    
-    // MARK: - MOUITestDemo (相关)
-    @objc func clickSend(sender: UIButton) {
-        print("click send button");
-    }
-    
 
 }
--- MOSurveySwift/MOSurveySwift/ViewController.swift
+++ MOSurveySwift/MOSurveySwift/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     setupView()
 //    test()
+    navigationController?.pushViewController(MOUITestsViewController(), animated: true)
   }
   
   private func setupView() {
